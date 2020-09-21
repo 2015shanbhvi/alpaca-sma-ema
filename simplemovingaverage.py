c89c91d0745d180550d7074e43b30c323f0c655b
Vinay Shanbhag <vinay.shanbhagibm.com@Vinays-MacBook-Pro-2.local>
2020-09-21 15:06:30 -0500

    optimized with moving update position isntead of push and popping values

--- simplemovingaverage.py
+++ simplemovingaverage.py
@@ -3,22 +3,27 @@ import statistics
 class SMA:
 	def __init__(self, sma_num):
 		self.sma_num = sma_num
-		self.sma_arr = []
+		self.sma_arr = [0]*sma_num
 		self.sma = 0
+		self.pos = 0
+		self.ready = False
 
 
 	def add_to_SMA(self, price):
-		#manage the length
-		if len(self.sma_arr) > self.sma_num - 1:
-			self.sma_arr.pop(0)
-		#add to it and recalculate average
-		self.sma_arr.append(price)
+
+		#replacement strategy instead of pop and append
+		if self.pos > self.sma_num - 1:
+			self.ready = True
+			self.pos = 0
+		self.sma_arr[self.pos] = price
+		self.pos += 1
+
 		self.sma = round(statistics.mean(self.sma_arr), 2)
 
-		#print value, or if not ready yet
-		if len(self.sma_arr) < self.sma_num:
-			print("SMA", self.sma_num, "has ", len(self.sma_arr), "datapoints and is not ready yet")
-		else:
-			print("SMA", self.sma_num, ": ", self.sma)
-
+		
+		if self.ready:
+			print("The SMA", self.sma_num, " is ready:", self.sma)
 	
+
+
+#Inheritance
