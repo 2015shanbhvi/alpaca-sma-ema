02f4f2166a138fa6a58306fb7b71c02c3c10d923
Vinay Shanbhag <2015shanbhvi@gmail.com>
2020-09-26 15:18:57 -0500

    added flask functionality, exposed routes

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 from stream import *
+from threading import Thread
+from flask import Flask, request
+from flask_restful import Api
+from flask_jsonpify import jsonify
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@app.route('/')
+def hello():
+   return "Welcome to the Alpaca trade bot"
+
+@app.route('/getHistory')
+def get(): 
+	retval = "GET request from class History"
+	return jsonify(retval)
+
 
 def main():
 	print("********************************")
 	print("Welcome to my Alpaca trading bot")
 	print("********************************")
 	
-	stream()
+	# stream()
+	# app.run(port='8080')
+	Thread(target = stream).start()
+	Thread(target = app.run(port='8080')).start()
+
+	
 
 if __name__ == "__main__":
     main()
+
